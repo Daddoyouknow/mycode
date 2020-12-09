842af098a042abe3ecff06401a33ad3e902202e8
genavous bell <genavous.bell@tlgcohort.com>
2020-12-09 20:33:33 +0000

    custom if

---
+++ custif/cust_if_script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+  
+# create the string hostname
+question1 = input( "what is the capitol of Georgia: ")
+
+if question1 == "Atlanta":
+    print("The answer was 100% correct")
+    print("Good Job")
+
+elif question1 != "Atlanta":
+    print("WRONG!!think again")
+
+
