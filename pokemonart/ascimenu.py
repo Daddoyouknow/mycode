bca00c4d006771e0c708f9174d91e060a29c40dc
genavous bell <genavous.bell@tlgcohort.com>
2020-12-15 18:11:39 +0000

    ask4 proj help

--- pokemonart/ascimenu.py
+++ pokemonart/ascimenu.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
+#------------>SAM, I have a few things I need help with. Feel free to comment  out below. 
 
-#first we will diplay our pokemon title
-title = open("pokemontitle.ascii")
-print(title.read()) 
-print("Welcome to Pokemon Print!!""\n")
-#time.sleep(3)
+#1.i need to be able to start the loop over from any choice. so if the user chose 1, the picture displays, i dont want it to leave the scipt. i would like for it to ask the "quit or more" question then got through the loop to choose another pokemon.
+
+#2. when i press the number 10 in my choices it stops the script instead of asking for another option. i have a while loop at the bottom that says while choice > 6 it should ask to try again. this works as long as i dont go over 9.
+
+#3. overall i will be adding more information and making it look nice but i want to be sure im greacefully entering and exiting the script. PLEASE ADD any suggestions<-------------------### 
+
+#first we will define our pokemon title screen
+def main():
+    title = open("pokemontitle.ascii")
+    print(title.read()) 
+    print("Welcome to Pokemon Print!!""\n")
+    print("---------------------------------------------------------------""\n")
 
 #our user input
 
-print( "1.bulbasaur  2.charizard 3.charmander 4.charmeleon 5.pikachu 6.squirtle""\n" )
-choice = input( "which Pokemon would you like to see? choose a number:" )
-#print( "you would like to learn more about:" + choice) 
 
+main()
+choice = input( "1.bulbasaur  2.charizard 3.charmander 4.charmeleon 5.pikachu 6.squirtle""\n"" which Pokemon would you like to see? choose a number:" )
+#lets diplay the title 
 #this is our variables for pokemon images to choose from 
 bulb = open("bulbasaur.ascii")
 zard = open("charizard.ascii")
@@ -21,16 +29,11 @@ pika = open("pikachu.ascii")
 squirt = open("squirtle.ascii")
 
 #trying to make a while loop that will take the user back to the initial question if the input >7
-while choice > "6":
-   #print(input( "which Pokemon would you like to see? choose a number:" )
-    print( "1.bulbasaur  2.charizard 3.charmander 4.charmeleon 5.pikachu 6.squirtle""\n" )
-    choice = input( "Sorry?!? that wasn't an option. Please choose a number from above :" )
-#print( "you would like to learn more about:" + choice) 
-
 #this is our loop for display
 if choice == "1":
     print("here is an image of bulbasaur")
     print(bulb.read())
+#print( "you would like to learn more about:" + choice) 
 elif choice == "2":
     print("here is an image of charizard")
     print(zard.read())
@@ -46,6 +49,27 @@ elif choice == "5":
 elif choice == "6":
     print("here is an image of squirtle")
     print(squirt.read())
+
+while choice > "6":
+   #print(input( "which Pokemon would you like to see? choose a number:" )
+    print( "1.bulbasaur  2.charizard 3.charmander 4.charmeleon 5.pikachu 6.squirtle""\n" )
+    choice = input( "Sorry?!? that wasn't an option. Please choose a number from above :" )
+
+exit = input("if you would like to quit press q if you would like to see more press m: ")
+if exit == "q":
+    print("thank you for coming. Bye!!")
+    
+    
+
+#lets diplay the title 
+#print( "you would like to learn more about:" + choice)
+#I want to make it so if the choice is in range it gives more options
+   #print(input( "which Pokemon would you like to see? choose a number:" )
+    
+#print( "you would like to learn more about:" + choice)
+
+
+
 #else:                 # if answer was outside of choice range
        # print('Sorry. Try again!')
 
