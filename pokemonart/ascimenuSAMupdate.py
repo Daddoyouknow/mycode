2d3434cf80f14f91dc64c7d70cff4f65fec2e839
genavous bell <genavous.bell@tlgcohort.com>
2020-12-18 00:06:05 +0000

    updated ascimenuSam

--- pokemonart/ascimenuSAMupdate.py
+++ pokemonart/ascimenuSAMupdate.py
@@ -1,41 +1,56 @@
 #!/usr/bin/env python3
-#------------>SAM, I have a few things I need help with. Feel free to comment  out below. 
 
-#1.i need to be able to start the loop over from any choice. so if the user chose 1, the picture displays, i dont want it to leave the scipt. i would like for it to ask the "quit or more" question then got through the loop to choose another pokemon.
+#at least 5 more images..1 being a good bye image
+#look into sounds
+#loading animation
+#make reading files variables
+#take a look at screenshots to see if we can use dictionaries instead
 
-#2. when i press the number 10 in my choices it stops the script instead of asking for another option. i have a while loop at the bottom that says while choice > 6 it should ask to try again. this works as long as i dont go over 9.
 
-#3. overall i will be adding more information and making it look nice but i want to be sure im greacefully entering and exiting the script. PLEASE ADD any suggestions<-------------------### 
-
+import time
+from os import system
 #first we will define our pokemon title screen
 def main():
     title = open("pokemontitle.ascii")
     print(title.read()) 
     print("Welcome to Pokemon Print!!""\n")
     print("---------------------------------------------------------------""\n")
-    choice = input( "1.bulbasaur  2.charizard 3.charmander 4.charmeleon 5.pikachu 6.squirtle""\n"" which Pokemon would you like to see? choose a number:""\n""if you would like to Quit press q" )
-    if choice == "q":
-        break
-    if choice == "1":
-        print("here is an image of bulbasaur")
-        print(bulb.read())
-    #print( "you would like to learn more about:" + choice) 
-    elif choice == "2":
-        print("here is an image of charizard")
-        print(zard.read())
-    elif choice == "3":
-        print("here is an image of charmander")
-        print(mander.read())
-    elif choice == "4":
-        print("here is an image of charmeleon")
-        print(meleon.read())
-    elif choice == "5":
-        print("here is an image of pikachu")
-        print(pika.read())
-    elif choice == "6":
-        print("here is an image of squirtle")
-        print(squirt.read())
-    
+    while True:    
+        choice = input( "1.bulbasaur  2.charizard 3.charmander 4.charmeleon 5.pikachu 6.squirtle\n\nChoose the number of the Pokemon you would like to see or enter q to quit:")
+        if choice == "1":
+            system("clear")
+            print("Bulbasaur I Choose YOU!!")
+            print(bulb.read())
+        elif choice == "2":
+            system("clear")
+            print("Charizard I Choose YOU!!")
+            print(zard.read())
+        elif choice == "3":
+            system("clear")
+            print("Charmander I Choose YOU!!")
+            print(mander.read())
+        elif choice == "4":
+            system("clear")
+            print("Charemeleon I Choose YOU!!")
+            print(meleon.read())
+        elif choice == "5":
+            system("clear")
+            print("Pikachu I Choose YOU!!")
+            print(pika.read())
+        elif choice == "6":
+            system("clear")
+            print("Squirtle I Choose YOU!!")
+            print(squirt.read())
+        elif choice == "q":
+            system("clear")
+            #time.sleep(3)
+            print("\nPlease come again!!! Catch 'em All!!!") 
+            break
+        else:
+            print("\nSorry that wasn't an option. Please try again...\n ")
+
+
+
 #art = [{ "name1": "bulbasaur","ascii": "bulbasaur.ascii", "name2": "charizard", "ascii": "charizard.ascii"}] 
 
 #3.charmander 4.charmeleon 4.pikachu 5.squirtle" ]
@@ -50,7 +65,6 @@ pika = open("pikachu.ascii")
 squirt = open("squirtle.ascii")
 
 main() 
-leave()
 
 #trying to make a while loop that will take the user back to the initial question if the input >7
 #this is our loop for display
